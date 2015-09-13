ad5e458c2815d21391c99dcbf43355b973c3cf83
Piotr Karpala <pkarpala@infusion.com>
2015-09-13 23:59:09 +0200

    solution that is fast but fails test case 14/17 :|

--- KattisSolution/Program.cs
+++ KattisSolution/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using KattisSolution.IO;
 
 namespace KattisSolution
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Solve(Console.OpenStandardInput(), Console.OpenStandardOutput());
         }
@@ -18,18 +20,191 @@ namespace KattisSolution
             // uncomment when you need more advanced reader
             // scanner = new Scanner(stdin);
             // scanner = new LineReader(stdin);
-            BufferedStdoutWriter writer = new BufferedStdoutWriter(stdout);
+            var writer = new BufferedStdoutWriter(stdout);
 
             var n = scanner.NextInt();
-            int[] a = new int[n];
-            int[] b = new int[n];
+            var result = Solution2(n, scanner);
 
-            for (int i = 0; i < n; i++)
+
+            writer.Write(result);
+            writer.Write("\n");
+            writer.Flush();
+        }
+
+        private static string Solution2(int n, IScanner scanner)
+        {
+            var a = new int[n];
+            var b = new int[n];
+
+            for (var i = 0; i < n; i++)
             {
                 a[i] = scanner.NextInt();
             }
 
-            for (int i = 0; i < n; i++)
+            for (var i = 0; i < n; i++)
+            {
+                b[i] = scanner.NextInt();
+            }
+
+            // first put hands in order
+            a = GetDiffs(a).ToArray();
+            b = GetDiffs(b).ToArray();
+
+            //            if (DirtyHack(ref a, ref b))
+            //                return "impossible";
+
+            a = Compact(a).ToArray();
+            b = Compact(b).ToArray();
+
+            bool isSolutionOk = false;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                // assume it's ok
+                isSolutionOk = true;
+
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[j] != b[(i + j) % a.Length])
+                    {
+                        i++;
+                        while (i < a.Length && j > 0 && (a[j] != b[(i + j) % a.Length] || a[j - 1] != b[(i + j - 1) % a.Length]))
+                        {
+                            i++;
+                        }
+                        if (i == a.Length)
+                        {
+                            // set it to false when it's not
+                            isSolutionOk = false;
+                            break;
+                        }
+                        else
+                        {
+                            // check again full array
+                            j = 0;
+                        }
+                    }
+                }
+
+                // exit when found
+                if (isSolutionOk)
+                    break;
+            }
+
+            return isSolutionOk ? "possible" : "impossible";
+        }
+
+        public static IEnumerable<int> Compact(int[] a)
+        {
+            int streak = 0;
+            int streakStart = -1;
+            int limit = a.Length;
+
+            for (int i = 1; i < limit; i++)
+            {
+                if (a[i] == a[i - 1])
+                {
+                    if (streakStart == -1)
+                    {
+                        streakStart = i - 1;
+                        streak++;
+
+                        // only for the begining
+                        if (i == 1)
+                        {
+                            // go backwards to check
+                            for (int j = a.Length - 1; j > 1; j--)
+                            {
+                                if (a[i] == a[j])
+                                {
+                                    streak++;
+                                    limit--;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    streak++;
+                }
+                else
+                {
+                    if (streak > 0)
+                    {
+                        // end of streak
+                        yield return -(a[i - 1] + streak);
+                    }
+
+                    if (i == 1)
+                        yield return a[i - 1];
+
+                    yield return a[i];
+                    streak = 0;
+                    streakStart = -1;
+                }
+            }
+        }
+
+        private static bool DirtyHack(ref int[] a, ref int[] b)
+        {
+            var aSorted = a.OrderBy(v => v).ToArray();
+            var bSorted = b.OrderBy(v => v).ToArray();
+
+            for (int i = 0; i < aSorted.Length; i++)
+            {
+                if (aSorted[i] != bSorted[i])
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static IEnumerable<int> GetDiffs(int[] a)
+        {
+            var diffs = a.OrderBy(v => v).ToArray();
+
+            for (int i = 1; i < diffs.Length; i++)
+            {
+                yield return diffs[i] - diffs[i - 1];
+            }
+
+            yield return 360000 - diffs[a.Length - 1] + diffs[0];
+        }
+
+        private static LinkedList<int> Shift(ref int[] array)
+        {
+            LinkedList<int> a = new LinkedList<int>(array);
+            int n = array.Length;
+            var shifted = 0;
+            var delta = -array[0];
+            var elementToMove = array.Length - 1;
+            // we get an array that is ascending, we need to check if values from the end can be moved to beginning
+
+            while (360000 - a.Last.Value + delta < a.Last.Value - a.Last.Previous.Value)
+            {
+                delta += 360000 - a.Last.Value - delta;
+                a.AddFirst(0);
+                a.RemoveLast();
+            }
+
+            return a;
+        }
+
+        private static string Solution1(int n, IScanner scanner)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var a = new int[n];
+            var b = new int[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                a[i] = scanner.NextInt();
+            }
+
+            for (var i = 0; i < n; i++)
             {
                 b[i] = scanner.NextInt();
             }
@@ -38,9 +213,12 @@ namespace KattisSolution
             a = a.OrderBy(v => v).ToArray();
             b = b.OrderBy(v => v).ToArray();
             string result = null;
+            sw.Stop();
+            Debug.WriteLine("Data read and sorted in " + sw.Elapsed);
 
-            for (int attempt = 0; attempt < n; attempt++)
+            for (var attempt = 0; attempt < n; attempt++)
             {
+                Debug.WriteLine("Attempt " + attempt);
                 if (CheckSolution(ref a, ref b, attempt))
                 {
                     result = "possible";
@@ -48,21 +226,21 @@ namespace KattisSolution
                 }
             }
 
-            writer.Write(result ?? "impossible");
-            writer.Write("\n");
-            writer.Flush();
+            return result ?? "impossible";
         }
 
         private static bool CheckSolution(ref int[] a, ref int[] b, int bIndex)
         {
-            int startDiff = Math.Abs(a[0] - b[bIndex]) % 360000;
+            var startDiff = Math.Abs(a[0] - b[bIndex]);
 
             startDiff = startDiff <= 180000 ? startDiff : 360000 - startDiff;
 
-            for (int i = 1; i < a.Length; i++)
+            for (var i = 1; i < a.Length; i++)
             {
-                var diff = Math.Abs(a[i] - b[(bIndex + i) % b.Length]) % 360000;
-                diff = diff <= 180000 ? diff : 360000 - diff;
+                var diff = Math.Abs(a[i] - b[(bIndex + i) % b.Length]);
+
+                if (diff > 180000)
+                    diff = 360000 - diff;
 
                 if (diff != startDiff)
                     return false;
--- KattisSolution/Tests/CustomTest.cs
+++ KattisSolution/Tests/CustomTest.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Text;
 using NUnit.Framework;
 
@@ -23,6 +25,97 @@ namespace KattisSolution.Tests
                 // Assert
                 Assert.That(result, Is.EqualTo(expectedAnswer));
             }
+        }
+
+        [Test]
+        public void LongSampleTest_WithStringData_Should_Pass()
+        {
+            // Arrange
+            const string expectedAnswer = "possible\n";
+            StringBuilder sb1 = new StringBuilder();
+            for (int i = 0; i < 199999; i++)
+            {
+                sb1.Append((int)Math.Ceiling(i + 360000 / 199999f));
+                sb1.Append(" ");
+            }
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("200000\n359999 " + sb1 + "\n" + sb1 + "359999")))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void LongSampleTest_WithStringData_Should_Pass2()
+        {
+            // Arrange
+            const string expectedAnswer = "impossible\n";
+            StringBuilder sb1 = new StringBuilder();
+            const int sb1Start = 1;
+            StringBuilder sb2 = new StringBuilder();
+            const int sb2Start = 359999;
+            int sb1Prev = 0, sb2Prev = 0;
+            for (int i = 0; i < 179999; i++)
+            {
+                sb1.Append(sb1Start + 2 * i);
+                sb1.Append(" ");
+
+                if (i == 179998)
+                {
+                    sb2.Append(0);
+                }
+                else
+                {
+                    sb2.Append(sb2Start - 2 * i);
+                }
+                sb2.Append(" ");
+
+            }
+
+            using (var input = new MemoryStream(Encoding.UTF8.GetBytes("179999\n" + sb1 + "\n" + sb2)))
+            using (var output = new MemoryStream())
+            {
+                // Act
+                Program.Solve(input, output);
+                var result = Encoding.UTF8.GetString(output.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expectedAnswer));
+            }
+        }
+
+        [Test]
+        public void GetDiffs_Should_ReturnDiffsBetweenHands()
+        {
+            // Arrange
+            int[] data = new[] { 0, 358000, 1000, 359000, 2000, 90000 };
+            int[] expected = new[] { 1000, 1000, 88000, 268000, 1000, 1000 };
+
+            // Act
+            var result = Program.GetDiffs(data).ToArray();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Compact_Should_CompactArray()
+        {
+            // Arrange
+            int[] data = new[] { 100000, 100000, 100000, 359000, 2000, 90000 };
+            int[] expected = new[] { -100003, 359000, 2000, 90000 };
+
+            // Act
+            var result = Program.Compact(data).ToArray();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
         }
     }
 }
